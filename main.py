913b7dc9c47b8426fef3ba29746529294f28bc24
Isabella Ferranti <ibf@andrew.cmu.edu>
2025-05-03 21:08:43 +0000

    Rewrite code in main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import xml.etree.ElementTree as ET
+import csv
+import pandas as pd
+import os
+import re
+import glob
+import shutil
+from pathlib import Path
+import logging
+
+
 def main():
     print("Hello from crafting-software-final-project!")
 
