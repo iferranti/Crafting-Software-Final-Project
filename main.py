78d0706d6239c245579bbe405cbb17a0e3a1e414
Isabella Ferranti <ibf@andrew.cmu.edu>
2025-05-03 21:12:48 +0000

    Add logging and function to get file path

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import shutil
 from pathlib import Path
 import logging
 
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+def get_file_path(file_name):
+    """Returns the full path of a file"""
+    return Path.cwd() / file_name
+
+#def read_nd2_file(nd2_file_path):
+
 
 def main():
     print("Hello from crafting-software-final-project!")
